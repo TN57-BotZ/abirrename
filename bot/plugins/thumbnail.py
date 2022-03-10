fb1765913a8c923bab1fedff2483f532484c4643
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2022-03-10 17:43:40 +0530

    Update thumbnail.py

--- bot/plugins/thumbnail.py
+++ bot/plugins/thumbnail.py
@@ -14,7 +14,7 @@ async def show_thumbnail(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     thumbnail = await db.get_thumbnail(m.from_user.id)
     if not thumbnail:
-        return await m.reply_text("You didn't set custom thumbnail!")
+        return await m.reply_text("You didn't set custom thumbnail!🤨")
     await c.send_photo(m.chat.id, thumbnail, caption="Custom Thumbnail",
                        reply_markup=types.InlineKeyboardMarkup(
                            [[types.InlineKeyboardButton("Delete Thumbnail",
@@ -25,7 +25,7 @@ async def show_thumbnail(c: Client, m: "types.Message"):
 @Client.on_message(filters.command("set_thumbnail") & filters.private & ~filters.edited)
 async def set_thumbnail(c: Client, m: "types.Message"):
     if (not m.reply_to_message) or (not m.reply_to_message.photo):
-        return await m.reply_text("Reply to any image to save in as custom thumbnail!")
+        return await m.reply_text("Reply to any image to save in as custom thumbnail!😊")
     if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
     await add_user_to_database(c, m)
@@ -45,4 +45,4 @@ async def delete_thumbnail(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await db.set_thumbnail(m.from_user.id, None)
     await m.reply_text("Okay,\n"
-                       "I deleted custom thumbnail from my database.")
+                       "I deleted custom thumbnail from my database.🚮")
